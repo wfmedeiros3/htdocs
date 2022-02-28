fde3204cda2c9269f79046a9c70423c640851cb8
wfmedeiros3 <64015990+wfmedeiros3@users.noreply.github.com>
2022-02-28 16:12:21 -0300

    tela de produtos

---
+++ application/controllers/restrita/Produtos.php
@@ -0,0 +1,45 @@
+<?php
+
+defined('BASEPATH') OR exit('Ação não permitida');
+
+class Produtos extends CI_Controller {
+
+    public function __construct() {
+        parent::__construct();
+
+        if (!$this->ion_auth->logged_in()) {
+            redirect('restrita/login');
+        }
+        
+    }
+
+    public function index() {
+
+
+        $data = array(
+            'titulo' => 'Produtos cadastradas',
+            'styles' => array(
+                'bundles/datatables/datatables.min.css',
+                'bundles/datatables/DataTables-1.10.16/css/dataTables.bootstrap4.min.css',
+            ),
+            'scripts' => array(
+                'bundles/datatables/datatables.min.js',
+                'bundles/datatables/DataTables-1.10.16/js/dataTables.bootstrap4.min.js',
+                'bundles/jquery-ui/jquery-ui.min.js',
+                'js/page/datatables.js'
+            ),
+            'produtos' => $this->produtos_model->get_all(),
+        );
+
+      // echo '<pre>';
+      // print_r($data['produtos']);
+      // exit();
+
+
+        $this->load->view('restrita/layout/header', $data);
+
+        $this->load->view('restrita/produtos/index');
+
+        $this->load->view('restrita/layout/footer');
+    }
+}
--- application/views/restrita/layout/sidebar.php
+++ application/views/restrita/layout/sidebar.php
@@ -30,6 +30,10 @@
                     <li><a class="nav-link" href="<?php echo base_url('restrita/categorias') ?>">Categorias filhas</a></li>
                 </ul>
             </li>
+            
+            <li class="dropdown <?php echo $this->router->fetch_class() == 'produtos' && $this->router->fetch_method() == 'index' ? 'active' : ''; ?>">
+                <a href="<?php echo base_url('restrita/produtos'); ?>" class="nav-link"><i data-feather="archive"></i><span>Produtos</span></a>
+            </li>
 
             <li class="dropdown">
                 <a href="#" class="menu-toggle nav-link has-dropdown"><i
